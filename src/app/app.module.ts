ea42df19383031b549d757c016c86260d1bd948e
Lucky Hlakaza <nhlalucky9@gmail.com>
2018-04-18 10:49:14 +0200

    feat(chore): initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { SliderComponent } from './components/slider/slider.component';
+import { SlideItemComponent } from './components/slide-item/slide-item.component';
+import { SlideComponent } from './components/slide/slide.component';
+import { DotsComponent } from './components/dots/dots.component';
+import { ArrowComponent } from './components/arrow/arrow.component';
+import { SwiperDirective } from './directives/swiper.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    SlideComponent,
+    SlideItemComponent,
+    DotsComponent,
+    ArrowComponent,
+    SwiperDirective
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SwiperDirective
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
